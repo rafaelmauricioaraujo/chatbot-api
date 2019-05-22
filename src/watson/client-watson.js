0604af6208e032f594d03f7822c5f2d568d875fe
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-05-22 18:14:29 -0300

    tentantiva usando configuração de autenticacao desativada

--- src/watson/client-watson.js
+++ src/watson/client-watson.js
@@ -2,8 +2,9 @@ const AssistantV1 = require('ibm-watson/assistant/v1');
 
 const watsonAssistant = new AssistantV1({
   version: '22-05-2019',
-  iam_apikey: '{LNU3eGMtyLcjFhTUaaph-QdR403Bhe_4K2f-S1N8thFe}',
-  url: '{https://gateway.watsonplatform.net/assistant/api}'
+  iam_apikey: 'LNU3eGMtyLcjFhTUaaph-QdR403Bhe_4K2f-S1N8thFe',
+  url: 'https://gateway.watsonplatform.net/assistant/api',
+  disable_ssl_verification: true,
 });
 
 module.exports = watsonAssistant;
